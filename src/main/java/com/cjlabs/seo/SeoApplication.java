4c3a831f26ce01dc09e7c231cd389dbfe53fdd21
jamie20241210 <jamie20241210@gmail.com>
2026-03-30 22:18:56 +0700

    init

--- src/main/java/com/cjlabs/seo/SeoApplication.java
+++ src/main/java/com/cjlabs/seo/SeoApplication.java
@@ -4,6 +4,6 @@ package com.cjlabs.seo;
 public class SeoApplication {
 
     public static void main(String[] args) {
-        ApplicationContextRunnerWrapper.run(RemoteApplication.class, args);
+        ApplicationContextRunnerWrapper.run(SeoApplication.class, args);
     }
 }
